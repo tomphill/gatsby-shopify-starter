fca3e98744da1c122efa8dd885c17bb6c14c18f9
tom <tomphill@live.co.uk>
2020-07-14 12:57:18 +0800

    Add global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from 'react';
+import { GlobalStyle } from './src/components/globalStyles';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <>
+    <GlobalStyle />
+    {element}
+  </>
+);
