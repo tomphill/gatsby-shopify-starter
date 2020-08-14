f9ceac60637570c6f9f79dd3231a61738b3c0bf1
tom <tomphill@live.co.uk>
2020-08-14 11:35:26 +0800

    remove unused deps. update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       options: {
         shopName: process.env.GATSBY_SHOP_NAME,
         accessToken: process.env.GATSBY_ACCESS_TOKEN,
-        apiVersion: '2020-04',
+        apiVersion: '2020-07',
       },
     },
     `gatsby-plugin-react-helmet`,
@@ -45,7 +45,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-shopify-starter`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
